de1c7598260a65aef3c20d13671cc183ed36ca30
shanghuifu <shanghuifu@gmail.com>
2019-10-22 20:04:19 +0900

    convert_erb2haml
    haml-railsというgemを導入し、erbファイルをhamlに変換

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ChatSpace
       g.stylesheets    false
       g.javascripts    false
       g.helper         false
-      g,test_framework false
+      g.test_framework false
+    end
   end
 end
