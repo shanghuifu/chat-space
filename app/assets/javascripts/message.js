39c5e795c8676f6a7a02fb924ec8d4fcf35a49b2
shanghuifu <shanghuifu@gmail.com>
2019-11-04 17:30:16 +0900

    ajaxの実装およびcssの修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function(){
+
+  function buildMessage(message){
+    var image = ""
+    if(message.image.url != null) {
+      image = `<img class="message__content" src="${message.image.url}">`}
+    else {
+      image = ""
+    }
+    var html = `<div class="message">
+                  <div class="message__info">
+                    <p class="message__info__talker">${message.name}</p>
+                    <p class="message__info__date">${message.date}</p>
+                  </div>
+                  <div class="message__content">
+                    ${message.content}
+                  </div>
+                  ${image}
+                </div>`
+    return html
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -6,10 +27,19 @@ $(function(){
     $.ajax({
       url: url,
       type: 'POST',
-      data: formData,  
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
+    .done(function(message){
+      var html = buildMessage(message);
+      $(".messages").append(html);
+      $('#message_content').val('');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert("メッセージの送信に失敗しました");
+    })
   })
-})
+});
