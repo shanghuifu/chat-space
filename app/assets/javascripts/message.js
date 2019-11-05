b4bde972ad5f396cf8f638fbcc88c60742a7c3c7
shanghuifu <shanghuifu@gmail.com>
2019-11-05 19:36:00 +0900

    最新メッセージの一覧追加とset interval
    取得した最新のメッセージをブラウザのメッセージ一覧に追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,7 @@
 $(function(){
-
   function buildMessage(message){
     var image = ""
-    if(message.image.url != null ? image = `<img class="message__content" src="${message.image.url}">` : image = "");
+    if(message.image.url ? image = `<img class="message__content" src="${message.image.url}">` : image = "");
     var html = `
       <div class="message" data-id="${message.id}">
         <div class="message__info">
@@ -14,8 +13,34 @@ $(function(){
         </div>
         ${image}
       </div>`;
-    return html
+    return html;
   }
+
+  var reloadMessages = function() {
+    last_message_id = $('.message').attr('data-id').max();
+    console.log(last_message_id);
+    $.ajax({
+      url: 'api/messages',
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+      var insertHTML = '';
+      if (messages.length !== 0) {
+        messages.forEach(function(message) {
+          buildMessage(message);
+        });
+      } else {
+        return false;
+      }
+      $(".messages").append(html);
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -40,24 +65,6 @@ $(function(){
     })
   })
 
-  var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id = $('.message').attr('data-id').max();
-    console.log(last_message_id);
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: 'api/messages',
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      console.log('success');
-    })
-    .fail(function() {
-      console.log('error');
-    });
-  };
+  setInterval(reloadMessages, 5000);
+  
 });
