f0200fffa3836d601cae0bc4d783c4089db57e76
shanghuifu <shanghuifu@gmail.com>
2019-11-05 18:48:54 +0900

    edit message.js
    追加したapi/messages#indexをリクエストするよう編集

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,4 +39,25 @@ $(function(){
       alert("メッセージの送信に失敗しました");
     })
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message').attr('data-id').max();
+    console.log(last_message_id);
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: 'api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 });
