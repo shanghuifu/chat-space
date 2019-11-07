8a90caecb46a59bde1adb69374fae7f552362209
shanghuifu <shanghuifu@gmail.com>
2019-11-07 15:30:52 +0900

    bug fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(documetn).on('turbolinks:load', function(){
+$(document).on('turbolinks:load', function(){
   function buildMessage(message){
     var image = message.image.url ? `<img class="message__content" src="${message.image.url}">` : "";
     var html = `
